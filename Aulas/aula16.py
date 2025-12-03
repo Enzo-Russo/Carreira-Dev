b779f34610a428b94dc3e5d5f869edf14e1059f0
Enzo Russo <enzo.russoti@gmail.com>
2025-12-03 14:48:18 -0300

    Adicionando as aulas em uma pasta

---
+++ Aulas/aula16.py
@@ -0,0 +1,10 @@
+# condicionais
+
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print("Seu acesso ao sistema foi permitido")
+elif entrada == 'sair':
+    print("Acesso ao sistema encerrado")
+else:
+    print("Acesso negado")
