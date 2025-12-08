897bdbbdc573667d68dde2939fecee48cb7ccb04
Enzo Russo <enzo.russoti@gmail.com>
2025-12-08 15:03:43 -0300

    Exercício Calculadora

---
+++ Aulas/aula40.py
@@ -0,0 +1,55 @@
+# calculadora com while
+
+while True: 
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+    
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são invalidos')
+        continue
+    
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    print('Realizando sua conta, confira: ')
+
+    if operador == '+':
+        print(f'{num_1_float}+{num_2_float}=', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float}-{num_2_float}=', num_1_float - num_2_float)
+    elif operador == '/':
+        if num_2_float == 0:
+            print('Não é possível dividir por zero.')
+        else:
+            print(f'{num_1_float}/{num_2_float}=', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'{num_1_float}*{num_2_float}=', num_1_float * num_2_float)
+    else:
+        print('Erro desconhecido.')
+
+
+    sair = input('Quer sair? [s]im: ').lower().starstwith('s')1
+
+    if sair is True:
+        print('Saindo da Calculadora. Até mais!')
+        break
+
