e1d4d6da7acad0e70969646430212924ad6b9168
Enzo Russo <enzo.russoti@gmail.com>
2025-12-09 14:28:05 -0300

    Script ETL com soma total

--- etl.py
+++ etl.py
@@ -1,7 +1,10 @@
+
+total = 0
+
+print('Iniciando processamento de vendas...')
+
 with open('vendas.csv', 'r') as arquivo:
-
     for linha in arquivo:
-
         linha_limpa = linha.strip()
 
         if not linha_limpa:
@@ -12,9 +15,15 @@ with open('vendas.csv', 'r') as arquivo:
         if len(dados) < 2:
             print(f'Linha ignorada (formato incorreto): {linha_limpa}')
             continue
-        
+
         nome = dados[0]
         valor = dados[1]
-        print(f'Cliente: {nome} | Comprou: R$ {valor}')
+        try:
+            valor_float = float(valor.strip())
+            total += valor_float
+            print(f'Processando vendas de: {nome} | Valor: R$ {valor_float:.2f}')
+        except ValueError:
+            print(f'Erro ao converter valor para o cliente {nome}')
 
-print('Leitura finalizada')
+print('-' * 40)
+print(f'Valor total de vendas: R$ {total:.2f}')
